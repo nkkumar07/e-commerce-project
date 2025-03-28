6fc694b690a4a85953c1fd72eda2c65861b236df
nkkumar07 <nkkumawat7357@gmail.com>
2025-03-28 11:15:38 +0530

    Update categories.py

--- api/routes/categories.py
+++ api/routes/categories.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from api.database.connection import get_db
 from api.database.schemas.categories import CategoryCreate, CategoryUpdate, CategoryResponse
-from api.crud.categories import create_category, delete_category, update_category, get_categories
+from api.crud.categories import create_category, delete_category, update_category, get_all_categories
 
 router = APIRouter()
 
@@ -10,11 +10,6 @@ router = APIRouter()
 @router.post("/add", response_model=CategoryResponse)
 def add_category(category: CategoryCreate, db: Session = Depends(get_db)):
     return create_category(db, category)
-    
-# Get all categories
-@router.get("/all", response_model=list[CategoryResponse])
-def get_all_categories(db: Session = Depends(get_db)):
-    return get_categories(db)
 
 # Delete a category by its ID
 @router.delete("/delete/{category_id}", response_model=dict)
@@ -26,4 +21,7 @@ def delete_category_by_id(category_id: int, db: Session = Depends(get_db)):
 def update_category_by_id(category_id: int, category_data: CategoryUpdate, db: Session = Depends(get_db)):
     return update_category(db, category_id, category_data)
 
-
+# Get all categories
+@router.get("/all", response_model=list[CategoryResponse])
+def get_categories(db: Session = Depends(get_db)):
+    return get_all_categories(db)
